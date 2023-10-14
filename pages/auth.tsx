a81caad63f9eefb7193f36a938dba680d1904f08
Leslie.Gu <snowberg.prc@gmail.com>
2023-10-14 22:27:29 +1000

    complete auth part

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,11 @@
 import Input from "@/components/Input";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
 
 const Auth = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -14,14 +18,43 @@ const Auth = () => {
     );
   }, []);
 
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
+
   return (
     <div className='relative w-full h-full bg-[url(/images/hero.jpg)] bg-no-repeat bg-center bg-fixed bg-cover'>
       <div className='bg-black w-full h-full lg:bg-opacity-50'>
-        <nav className='px-32 py-5'>
+        <nav className='px-12 py-5'>
           <img src='/images/logo.png' alt='Logo' className='h-12' />
         </nav>
         <div className='flex justify-center '>
-          <div className='bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md'>
+          <div className='bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
             <h2 className='text-white text-4xl mb-8 font-semibold'>
               {variant === "login" ? "Sign in" : "Register"}
             </h2>
@@ -29,27 +62,30 @@ const Auth = () => {
               {variant === "login" ? null : (
                 <Input
                   label='User Name'
-                  onChange={(event) => setName(event?.target.value)}
+                  onChange={(event: any) => setName(event?.target.value)}
                   id='name'
                   value={name}
                 />
               )}
               <Input
                 label='Email'
-                onChange={(event) => setEmail(event?.target.value)}
+                onChange={(event: any) => setEmail(event?.target.value)}
                 id='email'
                 type='email'
                 value={email}
               />
               <Input
                 label='Password'
-                onChange={(event) => setPassword(event?.target.value)}
+                onChange={(event: any) => setPassword(event?.target.value)}
                 id='password'
                 type='password'
                 value={password}
               />
             </div>
-            <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+            <button
+              onClick={variant === "login" ? login : register}
+              className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'
+            >
               {variant === "login" ? "Log in" : "Sign up"}
             </button>
             <p className='text-neutral-500 mt-12 '>
